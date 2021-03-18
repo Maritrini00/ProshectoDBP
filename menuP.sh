b727b0621255bbd816403c6f16a7117a761d1aa8
Axel Rg <a329830@uach.mx>
2021-03-18 12:31:10 -0700

    Se agrega el punto numero 8 de los requerimientos del proyecto

--- menuP.sh
+++ menuP.sh
@@ -1,34 +1,57 @@
 #!/bin/bash
 opcion=$1
 
+_backToMenu(){
+	echo
+	read -p "Presione 1 para volver al menu anterior o 2 para finalizar la ejecucion: " des
+	
+	case "$des" in
+		1)
+		   _menuChiquis
+		;;
+		2)
+		   exit 0
+		;;
+	esac
+
+}
+
 _agregarInfo(){
     echo
     read -p "Que quieres agregar?: " texto
 
-    case "$opcion" in
+    case "$metod" in
         scrum)
             echo $texto >> scrum.inf
+            _backToMenu
         ;;
         xp)
             echo $texto >> xp.inf
+            _backToMenu
         ;;
         kanban)
             echo $texto >> kanban.inf
+            _backToMenu
         ;;
         crystal)
             echo $texto >> crystal.inf
+            _backToMenu
         ;;
         cascada)
             echo $texto >> cascada.inf
+            _backToMenu
         ;;
         espiral)
             echo $texto >> espiral.inf
+            _backToMenu
         ;;
         modelov)
             echo $texto >> modelov.inf
+            _backToMenu
         ;;
         *)
             echo "opcion no valida"
+            _backToMenu
         ;;
     esac
     echo
@@ -37,30 +60,38 @@ _agregarInfo(){
 _buscar(){
     echo
     read -p "Que estás buscando? " coso
-    case "$opcion" in
+    case "$metod" in
         scrum)
             grep $coso scrum.inf
+            _backToMenu
         ;;
         xp)
             grep $coso xp.inf
+            _backToMenu
         ;;
         kanban)
             grep $coso kanban.inf
+            _backToMenu
         ;;
         crystal)
             grep $coso crystal.inf
+            _backToMenu
         ;;
         cascada)
             grep $coso cascada.inf
+            _backToMenu
         ;;
         espiral)
             grep $coso espiral.inf
+            _backToMenu
         ;;
         modelov)
             grep $coso modelov.inf
+            _backToMenu
         ;;
         *)
             echo "opcion no valida"
+            _backToMenu
         ;;
     esac
     echo
@@ -69,60 +100,76 @@ _buscar(){
 _eliminarInfo(){
     echo
     read -p "Que eliminamos jefe? pero escriba bien, respete mayúsculas y acentos pls: " texto
-    case "$opcion" in
+    case "$metod" in
         scrum)
             sed -i /$texto/d ./scrum.inf
+            _backToMenu
         ;;
         xp)
             sed -i /$texto/d ./xp.inf
+            _backToMenu
         ;;
         kanban)
             sed -i /$texto/d ./kanban.inf
+            _backToMenu
         ;;
         crystal)
             sed -i /$texto/d ./crystal.inf
+            _backToMenu
         ;;
         cascada)
             sed -i /$texto/d ./cascada.inf
+            _backToMenu
         ;;
         espiral)
             sed -i /$texto/d ./espiral.inf
+            _backToMenu
         ;;
         modelov)
             sed -i /$texto/d ./modelov.inf
+            _backToMenu
         ;;
         *)
             echo "opcion no valida"
+            _backToMenu
         ;;
     esac
     echo
 }
 
 _leerBase(){
-    case "$opcion" in
+    case "$metod" in
         scrum)
             cat scrum.inf
+            _backToMenu
         ;;
         xp)
             cat xp.inf
+            _backToMenu
         ;;
         kanban)
             cat kanban.inf
+            _backToMenu
         ;;
         crystal)
             cat crystal.inf
+            _backToMenu
         ;;
         cascada)
             cat cascada.inf
+            _backToMenu
         ;;
         espiral)
             cat espiral.inf
+            _backToMenu
         ;;
         modelov)
             cat modelov.inf
+            _backToMenu
         ;;
         *)
             echo "opcion no valida"
+            _backToMenu
         ;;
     esac
     echo
@@ -130,7 +177,7 @@ _leerBase(){
 
 _menuChiquis(){
     echo
-    echo "Usted esta en la sección ${opcion}, seleccione la opción que desea utilizar."
+    echo "Usted esta en la sección ${metod}, seleccione la opción que desea utilizar."
     echo
     echo "  1. Agregar información"
     echo "  2. Buscar"
@@ -154,6 +201,7 @@ _menuChiquis(){
         ;;
         *)
             echo "opcion no valida"
+            _backToMenu
         ;;
     esac
 }
@@ -167,7 +215,7 @@ _agile(){
     echo "  3. Kanban"
     echo "  4. Crystal"
     echo
-    read -p "escribe la opcion (con letritas): " opcion
+    read -p "escribe la opcion (con letritas): " metod
 
     _menuChiquis
 }
@@ -180,7 +228,7 @@ _tradicionales(){
     echo "  2. Espiral"
     echo "  3. Modelo V"
     echo
-    read -p "escribe la opcion (con letritas): " opcion
+    read -p "escribe la opcion (con letritas): " metod
 
     _menuChiquis
 }
